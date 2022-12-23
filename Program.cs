4613c7819a78205530568600dad7ca2b471e8712
HagarElbakry99 <121252814+HagarElbakry99@users.noreply.github.com>
2022-12-23 15:19:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter start number = ");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter end number = ");
+            int end = int.Parse(Console.ReadLine());
+            for(int i=start;i<=end;i++)
+            {
+                int j;
+                for(j=2;j<=i;j++)
+                {
+                    if (i % j == 0)
+                        break;
+                }
+                if (j == i)
+                    Console.WriteLine(i +" Prime number ");
+            }
+        }
+    }
+}
